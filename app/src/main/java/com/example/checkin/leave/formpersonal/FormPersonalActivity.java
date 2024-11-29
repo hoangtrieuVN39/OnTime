29ecb0f9c34b6972a1ee4d4944a763de19ff2f37
Trieu <hoangtrieuvn39@gmail.com>
2024-11-29 19:46:23 +0700

    29/11

--- app/src/main/java/com/example/checkin/leave/formpersonal/FormPersonalActivity.java
+++ app/src/main/java/com/example/checkin/leave/formpersonal/FormPersonalActivity.java
@@ -272,12 +272,16 @@ public class FormPersonalActivity extends Activity implements OnFormClickListene
                                 }
 
                                 // Format ngày tháng
-                                String formattedStartDate = formatDateTime(startDate);
-                                String formattedEndDate = formatDateTime(endDate);
-                                String dateOff = formattedStartDate + " - " + formattedEndDate;
+                                System.out.println(startDate);
+                                System.out.println(endDate);
+
+//                                String formattedStartDate = formatDateTime(startDate);
+//                                String formattedEndDate = formatDateTime(endDate);
+//                                String dateOff = formattedStartDate + " - " + formattedEndDate;
+
 
                                 // Thêm vào danh sách
-                                listForms.add(new Form(leaveRequestID, leaveTypeName, formattedStartDate, formattedEndDate, reason, status, countShift));
+//                                listForms.add(new Form(leaveRequestID, leaveTypeName, formattedStartDate, formattedEndDate, reason, status, countShift));
                                 filteredForms.clear();
                                 filteredForms.addAll(listForms);
 
@@ -417,6 +421,7 @@ public class FormPersonalActivity extends Activity implements OnFormClickListene
                 String status = cursor.getString(statussIndex);
                 int countShift = cursor.getInt(CountshiftIndex);
 
+                System.out.println(leaveStartTime);
                 String formattedStartTime = formatDateTime(leaveStartTime);
                 String formattedEndTime = formatDateTime(leaveEndTime);
 
