adace825bb49c275f1637c6b976c365da747b986
Vanthuyen <125105464+Vanthuyen@users.noreply.github.com>
2024-12-25 00:31:35 +0700

    25/12

--- app/src/main/java/com/example/checkin/leave/ApproverBTAdapter.java
+++ app/src/main/java/com/example/checkin/leave/ApproverBTAdapter.java
@@ -27,65 +27,45 @@ public class ApproverBTAdapter extends BaseAdapter implements Filterable {
     Context AbtfContext;
     OnFormNameClickListener AbtfListener;
 
-    private final List<ApproverBT> originalList; // Danh sách gốc
-    private List<ApproverBT> filteredList; // Danh sách sau khi lọc
+    private final List<ApproverBT> originalList;
+    private List<ApproverBT> filteredList;
     private final LayoutInflater inflater;
     private Filter approverFilter;
     private int selectedPosition = -1;
     private boolean isItemSelected = false;
     private String selectedApproverName = null;
     private LinkedHashSet<String> selectedApprovers;
-//    private Set<String> listselectedApprovers;
-    private List<String> listApprovers;// Danh sách người phê duyệt
+    private List<String> listApprovers;
 
 
 
     public ApproverBTAdapter(Context context, ArrayList<ApproverBT> forms, OnFormNameClickListener listener) {
         this.AbtfContext = context;
         this.originalList = forms;
-        this.filteredList = new ArrayList<>(forms); // Khởi tạo danh sách lọc từ danh sách gốc
+        this.filteredList = new ArrayList<>(forms);
         this.AbtfListener = listener;
         this.selectedApprovers = new LinkedHashSet<>();
-//        this.listselectedApprovers = new HashSet<>();
         this.listApprovers = listApprovers;
         this.inflater = LayoutInflater.from(context);
         initFilter();
     }
 
-//    public void setSelectedApprovers(Set<String> selectedApprovers) {
-//        this.selectedApprovers = new HashSet<>(selectedApprovers); // Cập nhật danh sách người phê duyệt đã chọn
-//        notifyDataSetChanged(); // Cập nhật lại ListView
-//    }
-
-//    public void setSelectedApprovers(Set<String> selectedApprovers) {
-//        if (selectedApprovers != null) {
-//            this.selectedApprovers = new HashSet<>(selectedApprovers);
-//        } else {
-//            this.selectedApprovers.clear();
-//        }
-//        notifyDataSetChanged(); // Cập nhật lại ListView
-//    }
-
     public void setSelectedApprovers(Set<String> selectedApprovers) {
         this.selectedApprovers.clear();
-//        this.listselectedApprovers.clear();
         if (selectedApprovers != null) {
             this.selectedApprovers.addAll(selectedApprovers);
-//            this.listselectedApprovers.addAll(selectedApprovers); // Đồng bộ hóa
         }
-        notifyDataSetChanged(); // Cập nhật giao diện
+        notifyDataSetChanged();
     }
 
 
     @Override
     public int getCount() {
-        // Sử dụng filteredList để trả về số lượng item sau khi lọc
         return filteredList.size();
     }
 
     @Override
     public Object getItem(int i) {
-        // Lấy item từ danh sách đã được lọc
         return filteredList.get(i);
     }
 
@@ -104,7 +84,7 @@ public class ApproverBTAdapter extends BaseAdapter implements Filterable {
         ImageView checkmark = view.findViewById(R.id.tick_icon);
 
         Button btnApprover = view.findViewById(R.id.approvername_txt);
-        ApproverBT approver = filteredList.get(i); // Lấy item từ danh sách đã được lọc
+        ApproverBT approver = filteredList.get(i);
         String approverName = approver.getNameApproveform();
         btnApprover.setText(approver.getNameApproveform());
 
@@ -112,24 +92,24 @@ public class ApproverBTAdapter extends BaseAdapter implements Filterable {
 
         if (isAlreadySelected) {
             // Nếu người phê duyệt đã được chọn, làm mờ item
-            view.setAlpha(0.5f);  // Giảm độ sáng
-            btnApprover.setEnabled(false); // Không cho phép chọn lại
-            checkmark.setVisibility(View.GONE); // Ẩn dấu tick
+            view.setAlpha(0.5f);
+            btnApprover.setEnabled(false);
+            checkmark.setVisibility(View.GONE);
         } else {
             // Nếu người phê duyệt chưa được chọn
             if (isItemSelected) {
                 if (i == selectedPosition) {
                     checkmark.setVisibility(View.VISIBLE);
-                    view.setAlpha(1.0f); // Item được chọn sẽ hiển thị rõ
+                    view.setAlpha(1.0f);
                 } else {
                     checkmark.setVisibility(View.GONE);
-                    view.setAlpha(0.5f); // Các item khác bị mờ đi
-                    btnApprover.setEnabled(false); // Không cho phép chọn các item khác khi đã chọn 1 item
+                    view.setAlpha(0.5f);
+                    btnApprover.setEnabled(false);
                 }
             } else {
                 checkmark.setVisibility(View.GONE);
-                view.setAlpha(1.0f);  // Hiển thị rõ tất cả các item nếu không có gì được chọn
-                btnApprover.setEnabled(true);  // Cho phép chọn tất cả các item
+                view.setAlpha(1.0f);
+                btnApprover.setEnabled(true);
             }
         }
 
@@ -138,46 +118,26 @@ public class ApproverBTAdapter extends BaseAdapter implements Filterable {
             if (isItemSelected && i == selectedPosition) {
                 // Nếu item đang được chọn, nhấn lần nữa sẽ bỏ chọn
                 selectedPosition = -1;
-//                checkmark.setVisibility(View.GONE);
                 isItemSelected = false;
                 selectedApproverName = null;
                 selectedApprovers.remove(approverName);
             } else  {
                 // Nếu item chưa được chọn, chọn nó và cập nhật lại vị trí
                 selectedPosition = i;
-//                checkmark.setVisibility(View.VISIBLE);
                 isItemSelected = true;
                 selectedApproverName = approver.getNameApproveform();
-//                selectedApprovers.add(selectedApproverName);
-//                addApprover(selectedApproverName);
+
             }
-            notifyDataSetChanged(); // Cập nhật lại giao diện ListView
+            notifyDataSetChanged();
             AbtfListener.onFormNameClick(approver.getNameApproveform());
         });
 
         return view;
     }
-//    public void addApprover(String approverName) {
-//        if (!selectedApprovers.contains(approverName)) {
-//            selectedApprovers.add(approverName);
-//            notifyDataSetChanged();
-//            Log.e("addApprover", "Danh sách hiện tại: " + selectedApprovers);
-//        }
-//    }
-
-//    public void addApprover(String approverName) {
-//        if (!selectedApprovers.contains(approverName)) {
-//            selectedApprovers.add(approverName);
-//            listselectedApprovers.addAll(selectedApprovers);
-//            notifyDataSetChanged();
-//            Log.e("addApprover", "Danh sách hiện tại: " + listselectedApprovers);
-//        }
-//    }
 
     public void addApprover(String approverName) {
         if (!selectedApprovers.contains(approverName)) {
             selectedApprovers.add(approverName);
-//            listselectedApprovers.addAll(selectedApprovers);
             notifyDataSetChanged();
             Log.e("addApprover", "Danh sách hiện tại: " + selectedApprovers);
         }
@@ -185,9 +145,8 @@ public class ApproverBTAdapter extends BaseAdapter implements Filterable {
 
     public void removeApprover(String approverName) {
         if (selectedApprovers.contains(approverName)) {
-            selectedApprovers.remove(approverName);  // Xóa người phê duyệt khỏi selectedApprovers
-//            listselectedApprovers.remove(approverName); // Xóa người phê duyệt khỏi listselectedApprovers
-            notifyDataSetChanged(); // Cập nhật lại ListView
+            selectedApprovers.remove(approverName);
+            notifyDataSetChanged();
             Log.e("removeApprover", "Danh sách hiện tại: " + selectedApprovers);
         }
     }
@@ -212,7 +171,7 @@ public class ApproverBTAdapter extends BaseAdapter implements Filterable {
             protected FilterResults performFiltering(CharSequence constraint) {
                 FilterResults results = new FilterResults();
                 if (constraint == null || constraint.length() == 0) {
-                    results.values = new ArrayList<>(originalList); // Sao chép danh sách gốc
+                    results.values = new ArrayList<>(originalList);
                     results.count = originalList.size();
                 } else {
                     String filterPattern = constraint.toString().toLowerCase().trim();
@@ -231,7 +190,7 @@ public class ApproverBTAdapter extends BaseAdapter implements Filterable {
             @Override
             protected void publishResults(CharSequence constraint, FilterResults results) {
                 filteredList = (List<ApproverBT>) results.values;
-                notifyDataSetChanged(); // Cập nhật lại ListView
+                notifyDataSetChanged();
             }
         };
     }
@@ -241,4 +200,3 @@ public class ApproverBTAdapter extends BaseAdapter implements Filterable {
         return approverFilter;
     }
 }
-
